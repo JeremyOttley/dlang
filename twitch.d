487005dcf69f5015e7974b3aecbb5c90f7f08e6b
Jeremy Ottley <Gazaunga@gmail.com>
2022-11-07 15:29:32 -0500

    Update twitch.d

--- twitch.d
+++ twitch.d
@@ -1,1 +1,16 @@
+import std.string;
+import std.stdio;
+import std.process;
 
+// need a way to capture user input
+
+void main() {
+//user input goes here, binds to ch
+  write("Channel: ");
+  string channel = readln.strip;
+  string twitchUrl = format("https://twitch.tv/%s", channel);
+  auto cmd = execute(["streamlink", twitchUrl, "best"]);
+    if (cmd.status != 0)
+      writeln("Command failed:\n", cmd.output);
+    else writeln(cmd.output);
+}
