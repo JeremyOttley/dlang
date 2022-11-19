2d25928b6d45277b44900b95b0489463ada5b113
Jeremy Ottley <Gazaunga@gmail.com>
2022-11-18 23:35:19 -0500

    Update fizzBuzz.d

--- fizzBuzz.d
+++ fizzBuzz.d
@@ -4,10 +4,6 @@ import std.stdio : write, writeln, writefln, readln;
 import std.string : chomp;
 import std.conv : to;
 
-bool gtZero(int n) {
-    return n >= 0;
-}
-
 void fizzBuzz(uint n) {
     foreach (x; 1 .. n + 1) {
         if (!(x % 15)) {
