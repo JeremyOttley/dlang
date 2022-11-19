b9f144a5f37cf6202b70758f0062a01482bbaff9
Jeremy Ottley <Gazaunga@gmail.com>
2022-11-18 23:30:18 -0500

    Create fizzBuzz.d

---
+++ fizzBuzz.d
@@ -0,0 +1,29 @@
+module FizzBuzz;
+
+import std.stdio : write, writeln, writefln, readln;
+import std.string : chomp;
+import std.conv : to;
+
+bool gtZero(int n) {
+    return n >= 0;
+}
+
+void fizzBuzz(uint n) {
+        foreach (x; 1 .. n + 1) {
+          if (!(x % 15)) {
+                writeln("FizzBuzz");
+    } else if (!(x % 3)) {
+        writeln("Fizz");
+    } else if (!(x % 5)) {
+        writeln("Buzz");
+    } else {
+        writeln(x);
+    }
+  }
+}
+
+void main() {
+    write("Enter an integer: ");
+    auto n = readln.chomp.to!int;
+    fizzBuzz(n);
+}
