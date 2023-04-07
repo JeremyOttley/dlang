36e05980c6e104b2986118034625cd09cc7e9fa9
Jeremy Ottley <Gazaunga@gmail.com>
2023-04-07 11:01:25 -0400

    Create if.d

---
+++ if.d
@@ -0,0 +1,17 @@
+import std.stdio;
+
+void main() {
+
+	int age = 12;
+
+	if((age >= 5) && (age <= 6) {
+		writeln("Kindergarten");
+	} else if ((age >= 7) && (age <= 13)) {
+		writeln("Middle School");
+	} else if ((age >= 14) && (age <= 18)) {
+		writeln("High School");
+	} else {
+		writeln("Stay home");
+	}
+
+}
