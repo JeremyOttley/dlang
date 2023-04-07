3301485a90e72e0c9f6ba2fe432102b052914ac6
Jeremy Ottley <Gazaunga@gmail.com>
2023-04-07 11:03:45 -0400

    Create switch.d

---
+++ switch.d
@@ -0,0 +1,21 @@
+void main() {
+
+string lang = "French";
+
+switch(lang) {
+	case "Chile": case "Cuba":
+		writeln("Hola");
+		break;
+	case "France":
+		writeln("Bonjour");
+		goto case;
+	case "Japan":
+		writeln("Konnichiwa");
+		break;
+	default:
+		writeln("Hello");
+
+}
+
+
+}
