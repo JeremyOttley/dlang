85e66d50dd87f8e0cdbb965bfcdc86c337a18395
Jeremy Ottley <Gazaunga@gmail.com>
2023-04-13 13:35:31 -0400

    Create main.d

---
+++ SimpleHTTPServer/main.d
@@ -0,0 +1,32 @@
+//arsd-official:cgi
+
+static import std.file;
+import arsd.cgi;
+import std.format;
+import std.stdio;
+
+void myHTTPServer(Cgi cgi) 
+{
+    string url = cgi.pathInfo;
+    writefln("GET %s", url);
+    switch(cgi.pathInfo) 
+    {
+        case "/style.css":
+            cgi.setResponseContentType("text/css");
+            cgi.write("body { color: red; }");
+            break;
+
+        case "/":
+            cgi.setResponseContentType("text/html");
+            cgi.write(std.file.read("index.html"));
+            break;
+
+        default:
+            cgi.setResponseStatus(404);
+    }
+    cgi.close();
+}
+
+mixin GenericMain!myHTTPServer;
+
+//dub -- --port 8080
