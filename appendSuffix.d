af4a0de926eef926baafc9b703391dbf82721fb2
Jeremy Ottley <Gazaunga@gmail.com>
2023-04-16 19:46:34 -0400

    Create appendSuffix.d

---
+++ appendSuffix.d
@@ -0,0 +1,32 @@
+import std.stdio;
+import std.file;
+import std.path;
+import std.algorithm;
+import std.array;
+
+void append2ToFileName()
+{
+    auto dir = dirEntries("", SpanMode.shallow)
+		.filter!(a => a.isFile)
+		.map!(a => baseName(a.name))
+		.array;
+
+    foreach (entry; dir)
+    {
+            string fileName = stripExtension(entry);
+            string newName = fileName ~ "2" ~ extension(entry);
+            try 
+            {
+                rename(entry, newName);
+            }
+            catch (Exception e)
+            {
+                writeln("Error renaming file: ", e.msg);
+            }
+    }
+}
+
+void main()
+{
+    append2ToFileName();
+}
