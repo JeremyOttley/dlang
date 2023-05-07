f578a82c85cbcfcb37e9056d09e5ff53cc394957
Jeremy Ottley <Gazaunga@gmail.com>
2023-05-06 20:05:10 -0400

    Create input.d

---
+++ input.d
@@ -0,0 +1,18 @@
+//user input 1
+
+void main() {
+	char[] arrName;
+	write("What's your name: ");
+	readln(arrName);
+	writeln("Hello ", strip(arrName))
+}
+
+//user input 2
+
+void main() {
+	write("Enter first and last name: ");
+	string fName = strip(readln());
+	string fN, lN;
+	formattedRead(fName, "%s %s", fN, lN);
+	writeln("Hello ", fN, " ", lN);
+}
